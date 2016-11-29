9a3bc8841fdc52191236235187cb0d83a0836919
kiraamaa <kira.a.maclean@gmail.com>
2016-11-29 08:56:41 -0500

    Add authentication files, index.js
    Need to link sign up with rails to front end

---
+++ assets/auth/events.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const getFormFields = require(`../../../lib/get-form-fields`);
+const api = require('./api');
+const ui = require('./ui');
+
+const onSignUp = function (event) {
+  let data = getFormFields(event.target);
+  event.preventDefault();
+  api.signUp(data)
+    .then(ui.success)
+    .catch(ui.failure);
+  $('#myModal').modal("hide");
+};
+
+// const onSignIn = function (event) {
+//   event.preventDefault();
+//   let data = getFormFields(event.target);
+//   api.signIn(data)
+//     .then(ui.signInSuccess)
+//     .catch(ui.failure);
+//   $('#myModal2').modal("hide");
+//   $('.sign-message').hide();
+//   $('.new-message').show();
+// };
+//
+// const onSignOut = function (event) {
+//   event.preventDefault();
+//   api.signOut()
+//     .then(ui.success)
+//     .catch(ui.failure);
+// };
+//
+// const onChangePassword = function (event) {
+//   event.preventDefault();
+//   let data = getFormFields(this);
+//   api.changePassword(data)
+//     .then(ui.success)
+//     .catch(ui.failure);
+//   $('#myModal3').modal("hide");
+// };
+
+const addHandlers = () => {
+  $('.sign-up-form').on('submit', onSignUp);
+//   $('.sign-in-form').on('submit', onSignIn);
+//   $('.sign-out-button').on('click', onSignOut);
+//   $('.change-password-form').on('submit', onChangePassword);
+};
+
+module.exports = {
+  addHandlers,
+};
---
+++ assets/auth/ui.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const app = require ('../app');
+
+const success = (data) => {
+  console.log(data);
+};
+//
+// const signInSuccess = (data) => {
+//   // console.log(data);
+//   app.user = data.user;
+// };
+
+const failure = (error) => {
+  console.error(error);
+};
+
+// const changePassword = () => {
+//   // console.log ('Password changed');
+// };
+//
+// const signOutSuccess = () => {
+//   app.user = null;
+//   // console.log ('Signed out');
+// };
+
+
+module.exports = {
+  failure,
+  success,
+  // signInSuccess,
+  // changePassword,
+  // signOutSuccess
+};
